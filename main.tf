dbedbaec81e120471a0b67b92a74e31f32778a7c
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-10-03 16:53:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+
+terraform { 
+  cloud { 
+    
+    organization = "Test-Abhinav" 
+
+    workspaces { 
+      name = "github-actions" 
+    } 
+  } 
+}
+    resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing"
+       }
+     }
+ resource "null_resource" "example1" {
+       triggers = {
+         value = "A example resource that does nothing-5"
+       }
+     }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-abhinav-rg"
+  location = "West Europe"
+}
+
+import {
+  to = azurerm_resource_group.example
+  id = "/subscriptions/8fdb01dc-7e3e-489e-9d2b-1a89a45d6c13/resourceGroups/example-abhinav-rg"
+}
