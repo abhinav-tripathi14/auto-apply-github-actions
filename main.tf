4d191a7a8d4e5e5ca18b534c9d3555c75fec18d0
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2025-12-08 17:56:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
          value = "A example resource that does nothing"
        }
      }
- resource "null_resource" "example2" {
+ resource "null_resource" "example3" {
        triggers = {
          value = "A example resource that again does nothing"
        }
